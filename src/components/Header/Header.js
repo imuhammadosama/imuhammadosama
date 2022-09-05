cff99743ed72befd12a90b21ec3fdff2fe7e27d2
Muhammad Osama <imuhammadosama@icloud.com>
2022-09-05 19:01:12 +0500

    Hotjar added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -83,24 +83,7 @@ export default function () {
         >
           Resources
         </Link>
-        <Link
-          to='icons'
-          className={
-            selectedItem === 'Icons' ? 'menu-item-active' : 'menu-item'
-          }
-          onClick={() => setSelectedItem('Icons')}
-        >
-          Icons
-        </Link>
-        <Link
-          to='upload'
-          className={
-            selectedItem === 'Upload' ? 'menu-item-active' : 'menu-item'
-          }
-          onClick={() => setSelectedItem('Upload')}
-        >
-          Upload
-        </Link>
+
         <Link
           to='contact'
           className={
