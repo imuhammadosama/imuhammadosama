1ebbdce21883fc01bf76b1b70d0165db80509e72
Muhammad Osama <imuhammadosama@icloud.com>
2022-09-26 20:17:38 +0500

    Update

--- src/data/websites.js
+++ src/data/websites.js
@@ -23,28 +23,4 @@ export const websites = [
     img: 'https://res.cloudinary.com/imuhammadosama/image/upload/h_1281,q_75,w_720/v1656771979/Portfolio/juggernaut_zczfv5.webp',
     type: 'React.js',
   },
-  {
-    title: 'IconsCave',
-    link: 'https://iconscave.com',
-    img: 'https://res.cloudinary.com/imuhammadosama/image/upload/h_1281,q_75,w_720/v1660652168/Portfolio/iconscave_sys1rf.webp',
-    type: 'React.js',
-  },
-  {
-    title: 'Scroll Reddit',
-    link: 'https://scrollreddit.com',
-    img: 'https://res.cloudinary.com/imuhammadosama/image/upload/h_1281,q_75,w_720/v1660651503/Portfolio/scrollreddit.com_c4isux.webp',
-    type: 'React.js',
-  },
-  {
-    title: 'Medicosurveillance',
-    link: 'https://www.figma.com/proto/ZTulxdbQbJPI3uMoLfyWZe/Medicosurveillance?node-id=298%3A111&scaling=scale-down-width&page-id=0%3A1&starting-point-node-id=298%3A111&hide-ui=1',
-    img: 'https://res.cloudinary.com/imuhammadosama/image/upload/h_1281,q_75,w_720/v1660735875/Portfolio/medicosurveillance_iyya3p.webp',
-    type: 'Figma',
-  },
-  {
-    title: 'Dairy Farm Products',
-    link: 'https://www.figma.com/proto/GzzhtJELbdVPngzaSaTS5Y/Dairy-Farm-Products?node-id=218%3A2307&scaling=scale-down-width&page-id=0%3A1&starting-point-node-id=218%3A2307&hide-ui=1',
-    img: 'https://res.cloudinary.com/imuhammadosama/image/upload/h_1281,q_75,w_720/v1660736078/Portfolio/dfp_bokqd8.webp',
-    type: 'Figma',
-  },
 ];
