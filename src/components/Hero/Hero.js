e7b87839a0f8e70486fa263e1565e020cbc05a6c
Muhammad Osama <imuhammadosama@icloud.com>
2022-11-02 11:25:10 +0500

    Effects updated

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,11 +3,12 @@ export default function () {
   return (
     <div className='hero'>
       <div className='hero-text'>
-        <div className='hero-description'>Professional Website Designer</div>
+        <div className='hero-description'>Hello, I'm Osama,</div>
         <div className='hero-title'>
           <div className='hero-title-one'>
-            You want,
-            <br />I build.
+            Website Developer with
+            <br />
+            Designing Skills.
           </div>
         </div>
         <a
