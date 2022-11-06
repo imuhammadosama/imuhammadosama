47cf60647765f688bb82b14b89b5443b962bedb6
Muhammad Osama <imuhammadosama@icloud.com>
2022-11-06 13:20:35 +0500

    Few websites are removed.

--- src/data/websites.js
+++ src/data/websites.js
@@ -1,16 +1,9 @@
 export const websites = [
   {
-    title: 'Dashblack',
-    link: 'https://dashblack.com',
-    img: 'https://res.cloudinary.com/imuhammadosama/image/upload/h_1281,q_75,w_720/v1659108978/Portfolio/dashblack_okgp2i.webp',
-    type: 'Business',
-    build: 'WordPress',
-  },
-  {
     title: 'Kryptloc',
     link: 'https://kryptloc.com',
     img: 'https://res.cloudinary.com/imuhammadosama/image/upload/h_1281,q_75,w_720/v1656773766/Portfolio/kryptloc_j7zdu9.webp',
-    type: 'Startup',
+    type: 'Business',
     build: 'WordPress',
   },
   {
@@ -20,26 +13,12 @@ export const websites = [
     type: 'Mobile App',
     build: 'WordPress',
   },
-  {
-    title: 'Dairy Farm Products',
-    link: 'https://www.figma.com/proto/GzzhtJELbdVPngzaSaTS5Y/Dairy-Farm-Products?node-id=218%3A2307&scaling=scale-down-width&page-id=0%3A1&starting-point-node-id=218%3A2307&hide-ui=1',
-    img: 'https://res.cloudinary.com/imuhammadosama/image/upload/q_auto:eco/v1660736078/Portfolio/dfp_bokqd8.webp',
-    type: 'Business',
-    build: 'Figma',
-  },
 
   {
-    title: 'Melbourne Street Tours',
-    link: 'https://melbournestreettours.com/',
-    img: 'https://res.cloudinary.com/imuhammadosama/image/upload/q_auto:eco/v1665239012/Portfolio/Melbourne_Street_Tours_udnlcq.webp',
+    title: 'Dashblack',
+    link: 'https://dashblack.com',
+    img: 'https://res.cloudinary.com/imuhammadosama/image/upload/h_1281,q_75,w_720/v1659108978/Portfolio/dashblack_okgp2i.webp',
     type: 'Business',
-    build: 'WordPress',
-  },
-  {
-    title: 'JuggernautPK',
-    link: 'https://juggernautpk.com',
-    img: 'https://res.cloudinary.com/imuhammadosama/image/upload/h_1281,q_75,w_720/v1656771979/Portfolio/juggernaut_zczfv5.webp',
-    type: 'Startup',
     build: 'WordPress',
   },
 
@@ -49,12 +28,5 @@ export const websites = [
     img: 'https://res.cloudinary.com/imuhammadosama/image/upload/q_auto:eco/v1660652364/Portfolio/techrevive_jcvawb.webp',
     type: 'Business',
     build: 'WordPress',
-  },
-  {
-    title: 'Medicosurveillance',
-    link: 'https://www.medicosurveillance.com.au/',
-    img: 'https://res.cloudinary.com/imuhammadosama/image/upload/q_auto:eco/v1660735875/Portfolio/medicosurveillance_iyya3p.webp',
-    type: 'Business',
-    build: 'ReactJS',
   },
 ];
