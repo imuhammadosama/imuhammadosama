a307d1be5290be5e33499cd86729714912bdb129
Muhammad Osama <imuhammadosama@icloud.com>
2022-11-29 17:20:12 +0500

    Updated

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,7 +3,7 @@ export default function () {
   return (
     <div className='hero'>
       <div className='hero-text'>
-        <div className='hero-description'>Full Stack Developer & Designer</div>
+        <div className='hero-description'>Website Designer & Developer</div>
         <div className='hero-title'>
           <div className='hero-title-one'>
             <span className='hero-title-one-colored'>Building websites</span> at
