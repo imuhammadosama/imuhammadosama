8d6e496244ee08c677d6008fadb5f5bef36e6ae4
Muhammad Osama <themuhammadosama@gmail.com>
2023-01-03 19:08:57 +0500

    Updated

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,28 @@
 import './Footer.css';
+import Gmail from '../../assets/connect/gmail.svg';
+import WhatsApp from '../../assets/connect/whatsapp.svg';
+import Skype from '../../assets/connect/skype.svg';
+import Messenger from '../../assets/connect/messenger.svg';
 
 export default function () {
-  return <div className='footer'></div>;
+  return (
+    <div className='footer'>
+      <div className='flex center'>
+        <div className='connect'>
+          <a href='mailto:themuhammadosama@gmail.com' target='_blank'>
+            <img src={Gmail} />
+          </a>
+          <a href='https://wa.me/+923352522522' target='_blank'>
+            <img src={WhatsApp} />
+          </a>
+          <a href=''>
+            <img src={Skype} />
+          </a>
+          <a href='https://www.messenger.com/t/imuhammadosama' target='_blank'>
+            <img src={Messenger} />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
